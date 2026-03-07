4cafca806cbc5aeeffe836b75a344849ac6101fc
aLearningLad <mocmanca@gmail.com>
2026-03-07 10:48:00 +0200

    typescript script into html file

---
+++ client/src/gameplay/game.ts
@@ -0,0 +1,11 @@
+const board = document.querySelector("board");
+
+// show number one inside board, in white
+
+// create a single block
+const square = document.createElement("div");
+
+// add classname so we can style it
+square.classList.add("square");
+
+board?.appendChild(square);
