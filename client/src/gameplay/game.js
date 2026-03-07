85fdd180dbb23458a54dd472c02b398748608e71
aLearningLad <mocmanca@gmail.com>
2026-03-07 11:12:11 +0200

    progress

---
+++ client/src/gameplay/game.js
@@ -0,0 +1,14 @@
+"use strict";
+const board = document.querySelector(".board");
+const squareCount = 9;
+// show number one inside board, in white
+if (board) {
+    // create a single block
+    const square = document.createElement("div");
+    // add classname so we can style it
+    square.classList.add("square");
+    // create 9 squares
+    for (let i = 1; i <= squareCount; i++) {
+        board.appendChild(square);
+    }
+}
--- client/src/gameplay/game.ts
+++ client/src/gameplay/game.ts
@@ -1,11 +1,16 @@
-const board = document.querySelector("board");
+const board = document.querySelector(".board");
+const squareCount = 9;
 
 // show number one inside board, in white
+if (board) {
+  // create a single block
+  const square = document.createElement("div");
 
-// create a single block
-const square = document.createElement("div");
+  // add classname so we can style it
+  square.classList.add("square");
 
-// add classname so we can style it
-square.classList.add("square");
-
-board?.appendChild(square);
+  // create 9 squares
+  for (let i = 1; i <= squareCount; i++) {
+    board.appendChild(square);
+  }
+}
