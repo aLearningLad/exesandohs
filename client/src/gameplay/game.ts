565c3643159049000823dfd20baccb51138d5047
Thato <112733708+aLearningLad@users.noreply.github.com>
2026-03-08 12:32:02 +0200

    Clicking functionality (#1)
    * onclick added
    
    * colour changes on click
    
    * colours sorted per turn
    
    * picking logic outline
    
    * move tracking

--- client/src/gameplay/game.ts
+++ client/src/gameplay/game.ts
@@ -1,10 +1,56 @@
 const board: HTMLDivElement | null = document.querySelector(".board");
 const squareCount = 9;
+let playCount = 1;
+let player1Claimed: string[] = [];
+let player2Claimed: string[] = [];
 
 // show number one inside board, in white
 if (board) {
   const changeColourOnClick = (squareId: string): void => {
-    alert("Clicked square with ID: " + squareId);
+    playCount++;
+
+    const thisSquare = document.getElementById(squareId);
+
+    // if player has chosen this square before, abort
+    if (thisSquare?.hasChildNodes()) {
+      alert("This square has already been claimed");
+      return;
+    }
+
+    // if play count is even -> square turns pink & add O
+    if (playCount % 2 === 0) {
+      // this is player 2
+      player2Claimed.push(squareId);
+
+      // handle colour
+      thisSquare?.classList.add("evenCount");
+      thisSquare?.classList.remove("square");
+
+      // add 0
+      const Oh = document.createElement("p");
+      Oh.innerHTML = "X";
+      thisSquare?.appendChild(Oh);
+      return;
+    } else {
+      player1Claimed.push(squareId);
+
+      // if play count is odd => square turns green & add X
+      thisSquare?.classList.add("oddCount");
+      thisSquare?.classList.remove("square");
+
+      // add X
+      const Ex = document.createElement("p");
+      Ex.innerHTML = "O";
+      thisSquare?.appendChild(Ex);
+    }
+
+    // check player has won
+
+    // possible winning combos
+    // 1,2,3 OR 456 OR 789
+    // 1,5,9 OR 3,5,7
+
+    //
   };
 
   // create 9 squares
@@ -14,10 +60,10 @@ if (board) {
     const square = document.createElement("div");
 
     // give the square an id for when user clicks it
-    square.setAttribute("id", String(i));
+    square.setAttribute("id", String(i + 1));
 
     // add an event listener to each square so we can click & shii
-
+    square.addEventListener("click", () => changeColourOnClick(String(i + 1)));
     // add classname so we can style it
     square.classList.add("square");
     board.appendChild(square);
