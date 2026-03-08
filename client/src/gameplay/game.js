f0a5c81354b34c001ec6cc5aa04c06eb77c39a12
aLearningLad <mocmanca@gmail.com>
2026-03-08 11:46:03 +0200

    progress

--- client/src/gameplay/game.js
+++ client/src/gameplay/game.js
@@ -3,12 +3,13 @@ const board = document.querySelector(".board");
 const squareCount = 9;
 // show number one inside board, in white
 if (board) {
-    // create a single block
-    const square = document.createElement("div");
-    // add classname so we can style it
-    square.classList.add("square");
     // create 9 squares
-    for (let i = 1; i <= squareCount; i++) {
+    for (let i = 0; i < squareCount; i++) {
+        console.log("appending square");
+        // create a single block
+        const square = document.createElement("div");
+        // add classname so we can style it
+        square.classList.add("square");
         board.appendChild(square);
     }
 }
--- client/src/gameplay/game.ts
+++ client/src/gameplay/game.ts
@@ -1,16 +1,25 @@
-const board = document.querySelector(".board");
+const board: HTMLDivElement | null = document.querySelector(".board");
 const squareCount = 9;
 
 // show number one inside board, in white
 if (board) {
-  // create a single block
-  const square = document.createElement("div");
-
-  // add classname so we can style it
-  square.classList.add("square");
+  const changeColourOnClick = (squareId: string): void => {
+    alert("Clicked square with ID: " + squareId);
+  };
 
   // create 9 squares
-  for (let i = 1; i <= squareCount; i++) {
+  for (let i = 0; i < squareCount; i++) {
+    console.log("appending square");
+    // create a single block
+    const square = document.createElement("div");
+
+    // give the square an id for when user clicks it
+    square.setAttribute("id", String(i));
+
+    // add an event listener to each square so we can click & shii
+
+    // add classname so we can style it
+    square.classList.add("square");
     board.appendChild(square);
   }
 }
