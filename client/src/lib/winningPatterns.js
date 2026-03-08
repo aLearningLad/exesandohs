894e6214d31e8d40077df9cbbb0654b3e052a67a
aLearningLad <mocmanca@gmail.com>
2026-03-08 16:05:44 +0200

    winning patterns array

---
+++ client/src/lib/winningPatterns.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.winningPatternsArr = void 0;
+exports.winningPatternsArr = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 5, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [3, 5, 7],
+];
