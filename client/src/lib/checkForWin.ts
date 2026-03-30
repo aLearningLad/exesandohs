55072bbba6bc9e0842c71d46217bfb6aeed4998c
aLearningLad <mocmanca@gmail.com>
2026-03-30 21:47:01 +0200

    outline

--- client/src/lib/checkForWin.ts
+++ client/src/lib/checkForWin.ts
@@ -5,20 +5,22 @@
 
 const winningCombosMap = new Map<string, string>([]);
 
-export const checkForWin = (
-  patterns: string[][],
-  userClaims: string[],
-  currentTurn: number,
-) => {
-  for (let i = 0; i > patterns.length; i++) {
-    if (
-      patterns[i].includes(userClaims.join("")) ||
-      patterns[i].includes(userClaims.reverse().join(""))
-    ) {
-      alert("Player " + currentTurn + " wins!");
-      return;
-    } else {
-      alert("Player " + currentTurn + " has claimed " + userClaims);
-    }
-  }
-};
+// export const checkForWin = (
+//   patterns: string[][],
+//   userClaims: string[],
+//   currentTurn: number,
+// ) => {
+//   for (let i = 0; i > patterns.length; i++) {
+//     if (
+//       patterns[i].includes(userClaims.join("")) ||
+//       patterns[i].includes(userClaims.reverse().join(""))
+//     ) {
+//       alert("Player " + currentTurn + " wins!");
+//       return;
+//     } else {
+//       alert("Player " + currentTurn + " has claimed " + userClaims);
+//     }
+//   }
+// };
+
+export const checkForWin = () => {};
